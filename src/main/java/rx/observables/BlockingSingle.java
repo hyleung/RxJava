85de9fa327a903133141c0fa1ddf77e184eaccd7
Ho Yan Leung <hy.leung@gmail.com>
2015-08-29 16:44:49 -0700

    Re-implements in terms of toObservable().toBlocking().single()

--- src/main/java/rx/observables/BlockingSingle.java
+++ src/main/java/rx/observables/BlockingSingle.java
@@ -2,13 +2,9 @@ package rx.observables;
 
 import rx.Single;
 import rx.SingleSubscriber;
-import rx.Subscription;
 import rx.annotations.Experimental;
 import rx.internal.operators.BlockingOperatorToFuture;
-
-import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Future;
-import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * {@code BlockingSingle} is a blocking "version" of {@link Single} that provides blocking
@@ -47,31 +43,7 @@ public class BlockingSingle<T> {
 	 *  @return the value emitted by this {@code BlockingSingle}
 	 */
 	public T get() {
-		final AtomicReference<T> returnItem = new AtomicReference<T>();
-		final AtomicReference<Throwable> returnException = new AtomicReference<Throwable>();
-		final CountDownLatch latch = new CountDownLatch(1);
-		Subscription subscription = single.subscribe(new SingleSubscriber<T>() {
-			@Override
-			public void onSuccess(T value) {
-				returnItem.set(value);
-				latch.countDown();
-			}
-
-			@Override
-			public void onError(Throwable error) {
-				returnException.set(error);
-				latch.countDown();
-			}
-		});
-		awaitForComplete(latch, subscription);
-		Throwable throwable = returnException.get();
-		if (throwable != null) {
-			if (throwable instanceof RuntimeException) {
-				throw (RuntimeException)throwable;
-			}
-			throw new RuntimeException(throwable);
-		}
-		return returnItem.get();
+		return single.toObservable().toBlocking().single();
 	}
 
 	/**
@@ -81,25 +53,6 @@ public class BlockingSingle<T> {
 	 */
 	public Future<T> toFuture() {
 		return BlockingOperatorToFuture.toFuture(single.toObservable());
-	}
-
-	private void awaitForComplete(CountDownLatch latch, Subscription subscription) {
-		if (latch.getCount() == 0) {
-			// Synchronous observable completes before awaiting for it.
-			// Skip await so InterruptedException will never be thrown.
-			return;
-		}
-		// block until the subscription completes and then return
-		try {
-			latch.await();
-		} catch (InterruptedException e) {
-			subscription.unsubscribe();
-			// set the interrupted flag again so callers can still get it
-			// for more information see https://github.com/ReactiveX/RxJava/pull/147#issuecomment-13624780
-			Thread.currentThread().interrupt();
-			// using Runtime so it is not checked
-			throw new RuntimeException("Interrupted while waiting for subscription to complete.", e);
-		}
 	}
 }
 
